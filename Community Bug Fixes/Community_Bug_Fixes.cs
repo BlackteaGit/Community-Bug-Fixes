9cb4cc5a88d40b41711b12ea5ad788418b8ac887
BlackteaGit <realblacktea@gmail.com>
2021-06-30 19:33:26 +0200

    fixing: unable to use logistics room to stash resources from homebase cargobays to ship building resources pool. (now you can use "Unload cargo" on your homebase)

--- Community Bug Fixes/Community_Bug_Fixes.cs
+++ Community Bug Fixes/Community_Bug_Fixes.cs
@@ -6,6 +6,8 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using WTFModLoader;
 using WTFModLoader.Manager;
+using System.Linq;
+using System.Reflection;
 
 namespace Community_Bug_Fixes
 {
@@ -20,7 +22,10 @@ namespace Community_Bug_Fixes
 
 		//public static TriggerEvent thrown;
 
-		//fixing: monster getting invisible an lagging the game after quiting the game near monsters and reloading (in vanilla some animations fail to instantiate for some reason)
+		//fixing: monster getting invisible an lagging the game after quiting the game near monsters and reloading (in vanilla some animations fail to instantiate for some reason) 
+		/*
+		 * Obsolete. Fixed in 0.9 Build 26
+		 * 
 		[HarmonyPatch(typeof(Monster), "animateMovement")]
 		public class Monster_animateMovement
 		{
@@ -43,6 +48,7 @@ namespace Community_Bug_Fixes
 				return false; //instruction for harmony to supress executing the original method
 			}
 		}
+		*/
 
 		//fixing: game crash on spawning Budds ship. (in vanilla KillBuddQuestRev2.targets is not initiallized on adding values to it )
 		[HarmonyPatch(typeof(KillBuddQuestRev2), "test")]
@@ -163,18 +169,88 @@ namespace Community_Bug_Fixes
 		}
 
 		//fixing: using logistics room on a ship allows unintended use of logistics commands on the command source ship.
+		//fixing: unable to use logistics room to stash resources from homebase cargobays to ship building resources pool. (now you can use "Unload cargo" on your homebase)
 		[HarmonyPatch(typeof(LogisticsScreenRev3), "doRightClick")]
 		public class LogisticsScreenRev3_doRightClick
 		{
 
 			[HarmonyPrefix]
-			private static void Prefix(ref string opt, Ship ___selected)
+			private static void Prefix(LogisticsScreenRev3 __instance, ref string opt, Ship ___selected)
 			{	
-				if (opt != "" && ___selected.id == PLAYER.currentShip.id)
+				if (opt != "" && ___selected.id == PLAYER.currentShip.id && ___selected.id != PLAYER.currentGame.homeBaseId)
 				{
 					SCREEN_MANAGER.widgetChat.AddMessage("Invalid target. Command target ship has to be distinct from command source ship.", MessageTarget.Ship);
 					opt = "";
 				}
+				if (opt == "Unload cargo" && ___selected.id == PLAYER.currentGame.homeBaseId && PLAYER.currentSession.GetType() == typeof(BattleSessionSP))
+				{
+					var shipStores = CHARACTER_DATA.getCargoTabs();
+					List<string> cargoNames = CHARACTER_DATA.getCargoNames();
+					BindingFlags flags = BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static;
+					if (shipStores.Count > 0)
+					{
+						for (int i = 0; i < shipStores.Count; i++)
+						{
+							var openStorage = shipStores[i];
+							
+							string[] storageTabNames;
+							if (cargoNames.Count != shipStores.Count)
+							{
+								storageTabNames = new string[shipStores.Count];
+								for (int j = 0; j < shipStores.Count; j++)
+								{
+									storageTabNames[j] = "Tab " + j.ToString();
+								}
+							}
+							else
+							{
+								storageTabNames = cargoNames.ToArray();
+							}
+							var args = new object[] { openStorage };
+							typeof(LogisticsScreenRev3).GetMethod("stashResources", flags, null, new Type[] { typeof(Storage) }, null).Invoke(__instance, args);
+							CHARACTER_DATA.storeCargoTab(i, storageTabNames[i], args[0] as Storage);
+						}
+						opt = "";
+					}			
+				}
+			}
+		}
+
+		//fixing: unable to use logistics room to stash resources from homebase cargobays to ship building resources pool. (now you can use "Unload cargo" on your homebase)
+		[HarmonyPatch(typeof(LogisticsScreenRev3), "updateInput")]
+		public class LogisticsScreenRev3_updateInput
+		{
+			[HarmonyPrefix]
+			private static void Prefix(ref MouseState __state, MouseState ___oldMouse)
+			{
+				__state = ___oldMouse;
+			}
+
+
+			[HarmonyPostfix]
+			private static void Postfix(LogisticsScreenRev3 __instance, MouseState __state, bool ___drawSpawn, bool ___pause, ref DropDown ___activeMenu, ref Ship ___hover, ref Ship ___selected, Vector2 ___mousePos)
+			{
+				if (!___drawSpawn && !___pause && Mouse.GetState().RightButton == Microsoft.Xna.Framework.Input.ButtonState.Released && __state.RightButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed && ___activeMenu == null)
+				{
+					if (___hover == null)
+					{
+						Rectangle clickPos = new Rectangle((int)___mousePos.X, (int)___mousePos.Y, 1, 1);
+						if (clickPos.Intersects(PLAYER.currentShip.bBox) && PLAYER.currentShip.id == PLAYER.currentGame.homeBaseId)
+						{
+							___hover = PLAYER.currentShip;
+						}						
+						if (PLAYER.currentShip == ___hover)
+						{
+							___activeMenu = new DropDown(___mousePos);
+							if ((___hover.cosm != null && ___hover.cosm.cargoBays.Count > 0) || (___hover.data != null && ___hover.data.storage != null))
+							{
+								___activeMenu.addOption("Unload cargo", null);
+							}
+							___selected = ___hover;
+						}
+					}
+				}
+
 			}
 		}
 
